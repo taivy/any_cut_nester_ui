e409b20a82e9bbf15cb5e8ce3d816cc2d2bde798
taivy <polina.revk@gmail.com>
2022-01-16 16:56:08 +0300

    add CSS styling

--- app/forms.py
+++ app/forms.py
@@ -4,7 +4,7 @@ from wtforms.validators import DataRequired
 
 
 class NestRequestForm(FlaskForm):
-    items_quantity = StringField("Items quantity (comma separated, order same as uploaded files)",
+    items_quantity = StringField("Items quantity (comma separated, order same as uploaded files):",
                                  validators=[DataRequired()])
     bin_size_x = IntegerField("Bin size (x):", validators=[DataRequired()], default=3000)
     bin_size_y = IntegerField("Bin size (y):", validators=[DataRequired()], default=1000)
